3f59975afef35eddb1a7a58351d7b9d11edc4bac
Ian Scrol <imscrol@gmail.com>
2024-06-16 17:06:23 -0500

    Add UI for RPS

--- index.js
+++ index.js
@@ -9,53 +9,56 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(){
-    let choice = prompt("Type r for rock, p for paper, and s for scissors").toLowerCase();
-    if (choice == 'r') {
-        return "rock";
-    } else if (choice == 'p') {
-        return "paper";
-    } else if (choice == 's') {
-        return "scissors";
-    } else {
-        return getHumanChoice();
-    }
-}
 
 let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice){
     if (humanChoice == computerChoice){
-        console.log("It's a tie! " + humanChoice + " ties " + computerChoice + ".");
+        resultDisplay.innerText = "It's a tie! " + humanChoice + " ties " + computerChoice + ".";
     } else if ((humanChoice == "rock" && computerChoice == "scissors") || 
                 (humanChoice == "paper" && computerChoice == "rock") || 
                 (humanChoice == "scissors" && computerChoice == "paper")){
-        console.log("You win! " + humanChoice + " beats " + computerChoice + ".");
+        resultDisplay.innerText = "You win! " + humanChoice + " beats " + computerChoice + ".";
         humanScore++;
     } else if ((humanChoice == "rock" && computerChoice == "paper") || 
                 (humanChoice == "paper" && computerChoice == "scissors") || 
                 (humanChoice == "scissors" && computerChoice == "rock")){
-        console.log("You lost! " + computerChoice + " beats " + humanChoice + ".");
+        resultDisplay.innerText = "You lost! " + computerChoice + " beats " + humanChoice + ".";
         computerScore++;
     } else {
-        console.log("Invalid Input!")
+        resultDisplay.innerText = "Invalid Input!";
+    }
+    score.innerText = "You: " + humanScore + " Computer: " + computerScore + ".";
+    if (humanScore >= 5){
+        finalResult.innerText = "You win!";
+        rockButton.disabled = true;
+        paperButton.disabled = true;
+        scissorsButton.disabled = true;
+    } else if (computerScore >= 5){
+        finalResult.innerText = "The Computer wins!";
+        rockButton.disabled = true;
+        paperButton.disabled = true;
+        scissorsButton.disabled = true;
     }
 }
 
-function playGame(numRounds){
-    for (i = 0; i < numRounds; i++){
-        let humanChoice = getHumanChoice();
-        let computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
-    }
-    if (humanScore > computerScore){
-        console.log("You won! " + humanScore + " to " + computerScore + ".");
-    } else if (humanScore < computerScore){
-        console.log("You lost! " + humanScore + " to " + computerScore + ".");
-    } else {
-        console.log("It was a tie! " + humanScore + " to " + computerScore + ".");
-    }
-}
+const rockButton = document.getElementById("rockButton");
+const paperButton = document.getElementById("paperButton");
+const scissorsButton = document.getElementById("scissorsButton");
+const resultDisplay = document.getElementById("roundResult");
+const score = document.getElementById("score");
+const finalResult = document.getElementById("finalResult");
 
-playGame(5);
+rockButton.addEventListener("click", function() {
+    playRound("rock", getComputerChoice());
+})
+
+paperButton.addEventListener("click", function(){
+    playRound("paper", getComputerChoice());
+})
+
+scissorsButton.addEventListener("click", function(){
+    playRound("scissors", getComputerChoice());
+})
+
